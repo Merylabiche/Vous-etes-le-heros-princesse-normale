25c2a7241c761d0abeca317029d10d1e9f6e7dbc
Mery <Meryberbiche@gmail.com>
2021-09-29 21:18:08 -0400

    Fichiers

---
+++ script.js
@@ -0,0 +1,33 @@
+let chapitre1 = {
+  subtitle: "Le commencement",
+  text: "Tu es une princesse et tu te réveille d'un cauchemar sans personne à côté de toi. Le prince s'est enfui et tu veux le retrouver.",
+  img: "tiana_dort.jpg",
+};
+
+let chapitre2 = {
+  subtitle: "Les chemins",
+  text: "Tu es une princesse et tu te réveille d'un cauchemar sans personne à côté de toi. Le prince s'est enfui et tu veux le retrouver.",
+  img: "tiana_dort.jpg",
+};
+
+let chapitre3 = {
+  subtitle: "La Chine",
+  text: "Tu es une princesse et tu te réveille d'un cauchemar sans personne à côté de toi. Le prince s'est enfui et tu veux le retrouver.",
+  img: "tiana_dort.jpg",
+};
+
+let chapitre4 = {
+  subtitle: "Le commencement",
+  text: "Tu es une princesse et tu te réveille d'un cauchemar sans personne à côté de toi. Le prince s'est enfui et tu veux le retrouver.",
+  img: "tiana_dort.jpg",
+};
+
+let chapitre5 = {
+  subtitle: "Le commencement",
+  text: "Tu es une princesse et tu te réveille d'un cauchemar sans personne à côté de toi. Le prince s'est enfui et tu veux le retrouver.",
+  img: "tiana_dort.jpg",
+};
+
+let chaptersObj = {
+  le_commencement: chapitre1,
+};
